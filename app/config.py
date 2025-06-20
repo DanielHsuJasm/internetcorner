dab4b47d08bcf4c8fd1e96d6662ed395c870a28a
DanielHsuJasm <danielhsu1129@icloud.com>
2025-06-20 17:09:27 +0800

    test try

--- app/config.py
+++ app/config.py
@@ -20,3 +20,5 @@ class DevelopmentConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    #########################for test
